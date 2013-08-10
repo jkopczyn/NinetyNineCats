d24e47d8348029ebf7e656ecc3b94b4651a22ba1
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-09 22:53:32 -0700

    only let owner approve/deny rental requests

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,9 +1,9 @@
 class CatRentalRequestsController < ApplicationController
+  before_filter :require_cat_ownership!, :only => [:approve, :deny]
+  
   def approve
-    @rental_request = CatRentalRequest.find(params[:id])
-    @rental_request.approve!
-
-    redirect_to cat_url(@rental_request.cat_id)
+    current_cat_rental_request.approve!
+    redirect_to cat_url(current_cat)
   end
 
   def create
@@ -12,13 +12,28 @@ class CatRentalRequestsController < ApplicationController
   end
 
   def deny
-    @rental_request = CatRentalRequest.find(params[:id])
-    @rental_request.deny!
-
-    redirect_to cat_url(@rental_request.cat_id)
+    current_cat_rental_request.deny!
+    redirect_to cat_url(current_cat)
   end
 
   def new
     @rental_request = CatRentalRequest.new
+  end
+
+  private
+  def current_cat_rental_request
+    @rental_request = CatRentalRequest.includes(:cat).find(params[:id])
+  end
+
+  def current_cat
+    current_cat_rental_request.cat
+  end
+
+  def current_user_owns_cat?
+    current_cat.user_id == current_user_id
+  end
+
+  def require_cat_ownership!
+    redirect_to cat_url(current_cat) unless current_user_owns_cat?
   end
 end
