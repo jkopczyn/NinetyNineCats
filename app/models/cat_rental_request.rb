b33a7a856e18a28d1ab1dc388a5cec263593e83a
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-09 18:05:41 -0700

    add approve! method

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -23,26 +23,54 @@ class CatRentalRequest < ActiveRecord::Base
 
   validate :does_not_overlap_approved_request
 
+  def approve!
+    raise "not pending" unless self.status == "PENDING"
+    transaction do
+      self.status = "APPROVED"
+      self.save!
+
+      self.overlapping_pending_requests.each do |req|
+        req.status = "DENIED"
+        req.save!
+      end
+    end
+  end
+
   private
   def assign_pending_status
     self.status ||= "PENDING"
   end
 
-  def does_not_overlap_approved_request
-    return unless self.status == "APPROVED"
+  def overlapping_requests
     conditions = <<-SQL
       ((cat_id = :cat_id)
         AND (start_date < :end_date)
-        AND (end_date > :start_date)
-        AND (status = 'APPROVED'))
+        AND (end_date > :start_date))
     SQL
-    conflicts = CatRentalRequest.where(conditions, {
+
+    overlapping_requests = CatRentalRequest.where(conditions, {
       :cat_id => self.cat_id,
       :start_date => self.start_date,
       :end_date => self.end_date
     })
 
-    unless conflicts.empty?
+    if self.id.nil?
+      overlapping_requests
+    else
+      overlapping_requests.where("id != ?", self.id)
+    end
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where("status = 'APPROVED'")
+  end
+
+  def overlapping_pending_requests
+    overlapping_requests.where("status = 'PENDING'")
+  end
+
+  def does_not_overlap_approved_request
+    unless overlapping_approved_requests.empty?
       errors[:base] << "Request conflicts with existing approved request"
     end
   end
