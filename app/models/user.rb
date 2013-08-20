65f33efa8c60740dc46817ed7dd5a649f5148bc5
Ned Ruggeri <ruggeri@self-loop.com>
2013-08-19 21:05:59 -0700

    fix stupid mispelling!

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
     user.is_password?(password) ? user : nil
   end
 
-  def is_pasword?(password)
+  def is_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
