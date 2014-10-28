7ae5936c07d4b32a47ed420b08604b753b66e084
Ned Ruggeri <ruggeri@self-loop.com>
2014-10-28 13:30:59 -0700

    don't give link for rental request unless logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   # Expose current_user method to the views
   helper_method :current_user
+  helper_method :logged_in?
 
   private
   def require_no_user!
@@ -12,6 +13,10 @@ class ApplicationController < ActionController::Base
   def current_user
     return nil unless session[:session_token]
     @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 
   def login_user!(user)
